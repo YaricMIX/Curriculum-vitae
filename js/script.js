c80369a31c157b058240d4a2dc185b01f6d1d77d
YaricMIX <aroslavlivachov@gmail.com>
2023-04-25 20:07:01 +0200

    Rework the script from lesson 32

--- js/script.js
+++ js/script.js
@@ -72,22 +72,47 @@ function handleKeyPress(event) {
 }
 	window.addEventListener('keydown', handleKeyPress);
 
-fetch('https://api.github.com/users/YaricMIX/repos')
-.then(response => response.json())
-.then(data => {
-	data.forEach(repo => {
-		const projectList = document.getElementById('projects-list');
-		const li = document.createElement('li');
-		const a = document.createElement('a');
-		a.href = repo.html_url;
-		a.textContent = repo.full_name;
-		li.appendChild(a);
-		if (repo.description) {
-			const p = document.createElement('p');
-			p.textContent = repo.description;
-			li.appendChild(p);
+class GithubAPI {
+	constructor(token, username) {
+		this.token = token;
+		this.username = username;
+	}
+
+	async getRepos() {
+		const url = `https://api.github.com/users/${this.username}/repos`;
+		const headers = new Headers({
+		'Authorization': `Basic ${btoa(`${this.username}:${this.token}`)}`,
+		});
+		const response = await fetch(url, { headers });
+		const data = await response.json();
+	return data;
+	}
+}
+
+async function displayProjects() {
+	const token = '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';
+	const username = 'YaricMIX';
+	const github = new GithubAPI(token, username);
+	const projectsList = document.getElementById('projects-list');
+	try {
+	const repos = await github.getRepos();
+		repos.forEach((repo) => {
+		const { full_name, html_url, description } = repo;
+		const listItem = document.createElement('li');
+		const link = document.createElement('a');
+		link.href = html_url;
+		link.textContent = full_name;
+		listItem.appendChild(link);
+		if (description) {
+			const descriptionElem = document.createElement('p');
+			descriptionElem.textContent = description;
+			listItem.appendChild(descriptionElem);
 		}
-		projectList.appendChild(li);
+		projectsList.appendChild(listItem);
 	});
-})
-.catch(error => console.error(error));
+	} catch (error) {
+		console.error(error);
+		projectsList.textContent = 'Failed to load projects.';
+	}
+}
+window.addEventListener('load', displayProjects);
